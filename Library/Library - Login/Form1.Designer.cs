5083c897f2c9306cf6606e1215ddfe773c155ae6
Vladimír Demko <vladimir.demko@akademiasovy.sk>
2016-04-07 11:06:39 +0200

    Login form, registration form and admin interface - prepared

---
+++ Library/Library - Login/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Library___Login
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Username = new System.Windows.Forms.TextBox();
+            this.Registration = new System.Windows.Forms.Label();
+            this.Login = new System.Windows.Forms.Button();
+            this.Password = new System.Windows.Forms.TextBox();
+            this.ErrorMessage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(63, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // Username
+            // 
+            this.Username.Location = new System.Drawing.Point(122, 45);
+            this.Username.Name = "Username";
+            this.Username.Size = new System.Drawing.Size(100, 20);
+            this.Username.TabIndex = 2;
+            // 
+            // Registration
+            // 
+            this.Registration.AutoSize = true;
+            this.Registration.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Registration.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.Registration.Location = new System.Drawing.Point(72, 119);
+            this.Registration.Name = "Registration";
+            this.Registration.Size = new System.Drawing.Size(115, 15);
+            this.Registration.TabIndex = 4;
+            this.Registration.Text = "Or register here!";
+            this.Registration.Click += new System.EventHandler(this.Registration_Click);
+            // 
+            // Login
+            // 
+            this.Login.Location = new System.Drawing.Point(100, 164);
+            this.Login.Name = "Login";
+            this.Login.Size = new System.Drawing.Size(75, 23);
+            this.Login.TabIndex = 5;
+            this.Login.Text = "Sign in";
+            this.Login.UseVisualStyleBackColor = true;
+            this.Login.Click += new System.EventHandler(this.Login_Click);
+            // 
+            // Password
+            // 
+            this.Password.Location = new System.Drawing.Point(122, 70);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(100, 20);
+            this.Password.TabIndex = 3;
+            this.Password.TextChanged += new System.EventHandler(this.Password_TextChanged);
+            // 
+            // ErrorMessage
+            // 
+            this.ErrorMessage.AutoSize = true;
+            this.ErrorMessage.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ErrorMessage.ForeColor = System.Drawing.Color.Red;
+            this.ErrorMessage.Location = new System.Drawing.Point(47, 104);
+            this.ErrorMessage.Name = "ErrorMessage";
+            this.ErrorMessage.Size = new System.Drawing.Size(205, 15);
+            this.ErrorMessage.TabIndex = 7;
+            this.ErrorMessage.Text = "Invalid username or password!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 223);
+            this.Controls.Add(this.ErrorMessage);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.Login);
+            this.Controls.Add(this.Registration);
+            this.Controls.Add(this.Username);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Username;
+        private System.Windows.Forms.Label Registration;
+        private System.Windows.Forms.Button Login;
+        private System.Windows.Forms.TextBox Password;
+        private System.Windows.Forms.Label ErrorMessage;
+    }
+}
+
---
+++ Library/Library - Login/RegistrationForm.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Library___Login
+{
+    public partial class RegistrationForm : Form
+    {
+        private string firstName, lastName, password, email, street, streetNumber, city;
+        private int postalCode;
+        private System.DateTime dateOfBirth = new DateTime();
+
+        public RegistrationForm()
+        {
+            InitializeComponent();
+        }
+
+        //button after click save the information to array, then, this information will be send to admin for confirm
+        private void Confirm_Click(object sender, EventArgs e)
+        {
+            firstName = FirstName.Text;
+            lastName = LastName.Text;
+            dateOfBirth = DateOfBirth.Value;
+            password = PasswordReg.Text;
+            email = EmailReg.Text;
+            street = Street.Text;
+            streetNumber = Number.Text;
+            city = City.Text;
+            postalCode = int.Parse(PostalCode.Text);
+            label1.Text = dateOfBirth.ToString();
+        }
+
+        //button after click remove all information and set the dateTimePicker to 1.1.2012
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            FirstName.Text = LastName.Text = EmailReg.Text = PasswordReg.Text = Street.Text = Number.Text = City.Text = PostalCode.Text = "";
+            DateOfBirth.Text = "1.1.2012"; 
+        }
+
+        //set the looks of char at password textbox as *
+        private void PasswordReg_TextChanged(object sender, EventArgs e)
+        {
+            PasswordReg.PasswordChar = '*';
+        }
+    }
+}
