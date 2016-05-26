96a5ba1d9f9293d62c288afdc05d2ec6902afe23
Vladimír Demko <vladimir.demko@akademiasovy.sk>
2016-05-26 14:29:12 +0200

    Admin can check reserved and lent books

---
+++ Library - Login/FormConfirmDeleting.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Library___Login
+{
+    partial class FormConfirmDeleting
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Message = new System.Windows.Forms.Label();
+            this.Confirm = new System.Windows.Forms.Button();
+            this.Refuse = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Message
+            // 
+            this.Message.AutoSize = true;
+            this.Message.Location = new System.Drawing.Point(36, 40);
+            this.Message.Name = "Message";
+            this.Message.Size = new System.Drawing.Size(35, 13);
+            this.Message.TabIndex = 0;
+            this.Message.Text = "label1";
+            // 
+            // Confirm
+            // 
+            this.Confirm.Location = new System.Drawing.Point(50, 118);
+            this.Confirm.Name = "Confirm";
+            this.Confirm.Size = new System.Drawing.Size(75, 23);
+            this.Confirm.TabIndex = 1;
+            this.Confirm.Text = "Yes";
+            this.Confirm.UseVisualStyleBackColor = true;
+            this.Confirm.Click += new System.EventHandler(this.Confirm_Click);
+            // 
+            // Refuse
+            // 
+            this.Refuse.Location = new System.Drawing.Point(151, 118);
+            this.Refuse.Name = "Refuse";
+            this.Refuse.Size = new System.Drawing.Size(75, 23);
+            this.Refuse.TabIndex = 2;
+            this.Refuse.Text = "No";
+            this.Refuse.UseVisualStyleBackColor = true;
+            this.Refuse.Click += new System.EventHandler(this.Refuse_Click);
+            // 
+            // FormConfirmEvent
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 153);
+            this.Controls.Add(this.Refuse);
+            this.Controls.Add(this.Confirm);
+            this.Controls.Add(this.Message);
+            this.Name = "FormConfirmEvent";
+            this.Text = "FormConfirmEvent";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Message;
+        private System.Windows.Forms.Button Confirm;
+        private System.Windows.Forms.Button Refuse;
+    }
+}
---
+++ Library - Login/FormConfirmDeleting.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Library___Login
+{
+    public partial class FormConfirmDeleting : Form
+    {
+        Connect2DB connection;
+        string UserID;
+
+        public FormConfirmDeleting(string message, string UserID) //more info is e.g. user id, for deleting user
+        {
+            InitializeComponent();
+            Message.Text = message;
+            this.UserID = UserID;
+            connection = new Connect2DB();
+        }
+
+        private void Confirm_Click(object sender, EventArgs e)
+        {
+            connection.deleteUserFromDatabase(UserID);
+            FormUserDetails.ActiveForm.Dispose();
+            this.Close();
+        }
+
+        private void Refuse_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
